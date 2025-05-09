c860c05f15aaedd15b4f06e9892ca8f2d5d643a2
Justwaname <26mas26igual50@gmail.com>
2025-05-09 12:00:26 -0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,7 +1,27 @@
 def suma(a, b):
     return a + b
 
-
 def test_suma():
     assert suma(2, 3) == 5
     assert suma(-1, 1) == 0
+
+def resta(a, b):
+    return a - b
+
+def test_resta():
+    assert resta(2, 3) == -1
+    assert resta(-1, 1) == -2
+
+def multiplicar(a, b):
+    return a * b
+
+def test_multiplicar():
+    assert multiplicar(2, 3) == 6
+    assert multiplicar(-1, 1) == -1
+
+def dividir(a, b):
+    return a / b
+
+def test_dividir():
+    assert dividir(4, 2) == 2
+    assert dividir(-1, 1) == -1
